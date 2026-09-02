3833ad0348f97fd17366b3a4274f5f4d358a52ab
baraazaka <bara.zaka@outlook.com>
2026-09-02 19:52:18 +0300

    add update category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
     getCategories,
     getCategory,
-    createCategory
+    createCategory,
+    updateCategory
 } from "../controllers/categoriesController.js";
 const router = express.Router();
 
 router.get("/", getCategories);
 router.get("/:id", getCategory);
 router.post("/", createCategory);
+router.put("/:id", updateCategory);
 export default router;
